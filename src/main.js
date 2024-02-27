214edb2ed5fc075818226d063331e1cefe1b0c31
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-02-27 16:08:56 +0100

    Added some comments.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+// Dieser import wird immer gebraucht wenn man etwas von Kaboom verwenden
+// möchte. Das ist eigentlich in fast allen Dateien der Fall.
+// Mit `k.function_name` kann dann die entsprechende Funktion von Kaboom
+// aufgerufen werden.
 import { k } from "./game.js"
 
+// Wenn wir eine neue Scene laden möchten, müssen wir diese zuerst importieren.
 import "./scenes/intro.js"
 
+// Mit der `k.go` Funktion, können wir Kaboom sagen zu welcher Scene wir als
+// nächstes gehen möchten. In diesem Fall laden wir die `intro` Scene.
 k.go("intro")
