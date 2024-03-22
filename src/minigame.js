d89cc76f27044a4cec83db0784954db287deddd8
Sara <e227538@edu.sbl.ch>
2024-03-22 11:39:21 +0100

    Minigame fertig

--- src/minigame.js
+++ src/minigame.js
@@ -10,39 +10,42 @@ const k = kaboom({
   canvas: document.getElementById("minigame"),
 })
 
-k.loadSpriteAtlas("sprites/char.png", {
-  hero: {
+k.loadSpriteAtlas("sprites/mushroom-char.png", {
+  mushroomhero: {
     // Alles war hier kommt, gehört zum Sprite `hero`
     x: 0, // x-Koordinate des Pixels wo das Sprite beginnt.
     y: 0, // y-Koordinate des Pixels wo das Sprite beginnt.
-    width: 3 * TILESIZE, // Die Breite des Sprites in Pixeln. Hier sind jeweils 3 Animationen nebeneinander, deshalb 3 * TILESIZE
-    height: 4 * TILESIZE, // Die Höhe des Sprites in Pixeln. Hier sind die 4 Laufrichtungen untereinander, deshalb 4 * TILESIZE
-    sliceX: 3, // In der x-Richtung sind es 3 Kacheln, so wird es gleichmässig aufgeteilt.
-    sliceY: 4, // In der y-Richtung sind es 4 Kacheln, so wird es gleichmässig aufgeteilt.
+    width: 2 * TILESIZE, // Die Breite des Sprites in Pixeln. Hier sind jeweils 3 Animationen nebeneinander, deshalb 3 * TILESIZE
+    height: 2 * TILESIZE, // Die Höhe des Sprites in Pixeln. Hier sind die 4 Laufrichtungen untereinander, deshalb 4 * TILESIZE
+    sliceX: 2, // In der x-Richtung sind es 3 Kacheln, so wird es gleichmässig aufgeteilt.
+    sliceY: 2, // In der y-Richtung sind es 4 Kacheln, so wird es gleichmässig aufgeteilt.
     anims: {
       // Hier werden die verschiedenen Animationen definiert.
-      runDown: { from: 0, to: 2, loop: true }, // Die Animation nach unten rennen, besteht aus den ersten 3 Kacheln. Die Animation soll sich wiederholen wenn sie durchlaufen ist.
-      idleDown: 1, // Ist es nur eine Kachel, kann diese direkt angegeben werden.
-      runLeft: { from: 3, to: 5, loop: true, speed: 10 }, // Die Geschwindigkeit der Animation kann auch verändert werden.
-      idleLeft: 4,
-      runRight: { from: 6, to: 8, loop: true },
-      idleRight: 7,
-      runUp: { from: 9, to: 11, loop: true, speed: 5 },
-      idleUp: 10,
+
+      down: 0, // Ist es nur eine Kachel, kann diese direkt angegeben werden.
+      left: 2,
+      right: 3,
+      up: 1,
     },
   },
 })
 
 const player = k.add([
-  k.sprite("hero", { anim: "idleRight" }),
+  k.sprite("mushroomhero", { anim: "down" }),
   k.pos(0, 0),
   k.body(),
   k.area(),
 ])
 
 k.onKeyPress("space", () => {
-  player.play("runRight")
+  player.play("right")
   setInterval(() => {
     player.move(k.RIGHT.scale(100))
   }, 1000 / 60)
+})
+
+player.onUpdate(() => {
+  if (player.pos.x > k.width() - TILESIZE) {
+    player.pos.x = 0
+  }
 })
